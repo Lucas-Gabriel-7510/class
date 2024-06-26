cc0ecc7e6ec4dd5814cead39b8a1db0c073ca3ef
Lucas-Gabriel-7510 <lucasgabrielnino08@gmail.com>
2024-06-26 16:30:46 +0100

    first commits

---
+++ Aula_1/script.js
@@ -0,0 +1,13 @@
+// alert("ola, mundo")
+
+console.log("oi")
+
+// Define a variável
+let idade = 17;
+
+// Verifica se a idade é maior ou igual a 18
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.");
+}
